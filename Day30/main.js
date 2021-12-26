4ea990108f80d13313f40a3e976b93767b2abd69
Karan Chandekar <karanchandekar431@gmail.com>
2021-12-26 17:44:49 +0530

    fizzBuzz

--- Day30/main.js
+++ Day30/main.js
@@ -1,42 +1,61 @@
 // BMI Calculator
-function bmiCalculator(weight, height) {
-  let bmi = weight / (height * height);
-  if (bmi < 18.5) {
-    return "Your BMI is " + bmi + ", so you are underweight.";
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    return "Your BMI is " + bmi + ", so you have a normal weight.";
-  } else {
-    return "Your BMI is " + bmi + ", so you are overweight.";
-  }
-}
-bmiCalculator(45, 5.2)
+// function bmiCalculator(weight, height) {
+//   let bmi = weight / (height * height);
+//   if (bmi < 18.5) {
+//     return "Your BMI is " + bmi + ", so you are underweight.";
+//   } else if (bmi >= 18.5 && bmi <= 24.9) {
+//     return "Your BMI is " + bmi + ", so you have a normal weight.";
+//   } else {
+//     return "Your BMI is " + bmi + ", so you are overweight.";
+//   }
+// }
+// bmiCalculator(45, 5.2)
 
 
 // Leap year Calculator
-function isLeap(year) {
-    if (year % 4 === 0) {
-      if (year % 100 === 0) {
-        if (year % 400) {
-          return "This is a leap year"
-        } else {
-          return "This is not a leap year"
-        }
-      } else {
-        return "This is a leap year"
-      }
-    } else {
-      return "This is not a leap year"
-    }
-  }
+// function isLeap(year) {
+//     if (year % 4 === 0) {
+//       if (year % 100 === 0) {
+//         if (year % 400) {
+//           return "This is a leap year"
+//         } else {
+//           return "This is not a leap year"
+//         }
+//       } else {
+//         return "This is a leap year"
+//       }
+//     } else {
+//       return "This is not a leap year"
+//     }
+//   }
 
-  isLeap(2019)
+//   isLeap(2019)
 
 
   // Array 
-  let guestList = ["Karan", "Vikrant", "Amit", "Amol", "Manish"]
-  var guestName = prompt("Please enter your name!")
-  if (guestList.includes(guestName)) {
-    alert("Welcome!")
-  } else {
-    alert("Sorry! may be next time")
+  // let guestList = ["Karan", "Vikrant", "Amit", "Amol", "Manish"]
+  // var guestName = prompt("Please enter your name!")
+  // if (guestList.includes(guestName)) {
+  //   alert("Welcome!")
+  // } else {
+  //   alert("Sorry! may be next time")
+  // }
+
+
+  // FizzBuzz using JavaScript
+  var output = []
+  var count = 1
+
+  function fizzBuzz() {
+    if (count % 3 === 0 && count % 5 === 0) {
+      output.push("FizzBuzz")
+    } else if (count % 3 === 0) {
+      output.push("Fizz")
+    } else if (count % 5 === 0) {
+      output.push("Buzz")
+    } else {
+      output.push(count)
+    }
+    count++
+    console.log(output)
   }
