39729d8fe33bc983ad04fd965b5ad1f62c70e55d
Karan Chandekar <karanchandekar431@gmail.com>
2021-12-26 16:38:20 +0530

    leap year calc

--- Day30/main.js
+++ Day30/main.js
@@ -1,13 +1,32 @@
 // BMI Calculator
-
 function bmiCalculator(weight, height) {
   let bmi = weight / (height * height);
   if (bmi < 18.5) {
-    console.log("Your BMI is " + bmi + ", so you are underweight.");
+    return "Your BMI is " + bmi + ", so you are underweight.";
   } else if (bmi >= 18.5 && bmi <= 24.9) {
-    console.log("Your BMI is " + bmi + ", so you have a normal weight.");
+    return "Your BMI is " + bmi + ", so you have a normal weight.";
   } else {
-    console.log("Your BMI is " + bmi + ", so you are overweight.");
+    return "Your BMI is " + bmi + ", so you are overweight.";
   }
 }
 bmiCalculator(45, 5.2)
+
+
+// Leap year bmiCalculator
+function isLeap(year) {
+    if (year % 4 === 0) {
+      if (year % 100 === 0) {
+        if (year % 400) {
+          return "This is a leap year"
+        } else {
+          return "This is not a leap year"
+        }
+      } else {
+        return "This is a leap year"
+      }
+    } else {
+      return "This is not a leap year"
+    }
+  }
+
+  isLeap(2019)
