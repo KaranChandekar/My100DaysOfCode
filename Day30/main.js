71a3690bff5e11b2f3fa81a17db25d157a5b73a8
Karan Chandekar <karanchandekar431@gmail.com>
2021-12-27 14:27:19 +0530

    add Js to website

--- Day30/main.js
+++ Day30/main.js
@@ -82,19 +82,36 @@
 
 // For loop
 // FizzBuzz using For loop
-var output = [];
+// var output = [];
 
-function fizzBuzz() {
-  for (let count = 1; count < 101; count++) {
-    if (count % 3 === 0 && count % 5 === 0) {
-      output.push("FizzBuzz");
-    } else if (count % 3 === 0) {
-      output.push("Fizz");
-    } else if (count % 5 === 0) {
-      output.push("Buzz");
-    } else {
-      output.push(count);
+// function fizzBuzz() {
+//   for (let count = 1; count < 101; count++) {
+//     if (count % 3 === 0 && count % 5 === 0) {
+//       output.push("FizzBuzz");
+//     } else if (count % 3 === 0) {
+//       output.push("Fizz");
+//     } else if (count % 5 === 0) {
+//       output.push("Buzz");
+//     } else {
+//       output.push(count);
+//     }
+//     console.log(output);
+//   }
+// }
+
+
+// fibonacci generator
+function fibonacciGenerator(n) {
+  var output = [];
+  if (n === 1) {
+    output = [0];
+  } else if (n === 2) {
+    output = [0, 1];
+  } else {
+    output = [0, 1];
+    for (var i = 2; i < n; i++) {
+      output.push(output[output.length - 2] + output[output.length - 1]);
     }
-    console.log(output);
   }
+  console.log(output)
 }
--- Day31/mian.js
+++ Day31/mian.js
@@ -1,15 +1,1 @@
-// fibonacci generator
-function fibonacciGenerator(n) {
-  var output = [];
-  if (n === 1) {
-    output = [0];
-  } else if (n === 2) {
-    output = [0, 1];
-  } else {
-    output = [0, 1];
-    for (var i = 2; i < n; i++) {
-      output.push(output[output.length - 2] + output[output.length - 1]);
-    }
-  }
-  console.log(output)
-}
+document.querySelector("h1").innerHTML = "Welcome to Javascript!"
