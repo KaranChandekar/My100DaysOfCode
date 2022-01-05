8d84f4282641481b61a673d23c123a06b12809af
Karan Chandekar <karanchandekar431@gmail.com>
2022-01-05 20:49:57 +0530

    add and remove element with jQuery

--- Day40/main.js
+++ Day40/main.js
@@ -14,3 +14,10 @@ $("body").keypress(function (event) {
 $("h1").on("mouseover", function () {
     $("h1").css("color", "purple")
 })
+
+$("h1").before("<button>New</button>")
+$("h1").after("<button>button</button>")
+$("h1").prepend("<button>Hello</button>")
+$("h1").append("<button>World</button>")
+
+$("button").remove()
